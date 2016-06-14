5d22d6ab0b407a4315f77807f173ecffe304d9c1
Andrii Popov <andrey.popov85@gmail.com>
2016-06-14 07:02:07 +0300

    Add files via upload

---
+++ src/com/goit/g2popov/module06/task01/NotANumberException.java
@@ -0,0 +1,11 @@
+package com.goit.g2popov.module06.task01;
+
+/**
+ * Exception class {@code NotANumberException} defines a case of NaN result of calculations
+ * This exception is thrown when we get NaN after extracting square root from a negative number.
+ * @author  Andrii Popov
+ */
+
+public class NotANumberException extends Exception {
+
+}
